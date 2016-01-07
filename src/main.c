a13ccc677623931f329c676b971851f0ba0f906e
luizperes <luiz_peres10@hotmail.com>
2016-01-06 19:26:54 -0800

    Arrows are working! :D

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 void init();
 void run();
 void kill();
-
 void init_colors();
+void handleKey(int c, R2Point* p);
 
 int main(int argc, char** argv)
 {
@@ -21,8 +21,11 @@ int main(int argc, char** argv)
 void init()
 {
   initscr();
+  cbreak();
   noecho();
   scrollok(stdscr, TRUE);
+  keypad(stdscr, TRUE);
+  refresh();
   init_colors();
 }
 
@@ -38,19 +41,50 @@ void run()
 {
   while(1)
   {
+    refresh();
+    int c = getch();
+
     if (!R2_SHOULD_NOT_USE_COLORS)
       attrset(COLOR_PAIR(rand() % 2));
-   
-    int curY, curX; 
-    getyx(stdscr, curY, curX);
     
-    int c = getch();
-    if (c == 10)
-      addch('\n');
-    else if (c == KEY_LEFT)
-     return;	
-    else if (c >= 32 && c <= 126)
-      addch(c);
+    R2Point point;
+    getyx(stdscr, point.y, point.x);
+ 
+    if (has_key(c))  	
+    {
+      handleKey(c, &point);
+      move(point.y, point.x);
+      continue;
+    }
+   
+    addch(c);
+  }
+}
+
+void handleKey(int c, R2Point* p)
+{
+  switch(c)
+  {
+    case KEY_LEFT:
+    {
+      p->x -= p->x > 0 ? 1 : 0;
+      break;
+    } 
+    case KEY_RIGHT:
+    {
+      p->x += p->x < COLS ? 1 : 0;
+      break;
+    }
+    case KEY_UP:
+    {
+      p->y -= p->y > 0 ? 1 : 0;
+      break;
+    }
+    case KEY_DOWN:
+    {
+      p->y += p->y < LINES ? 1 : 0;
+      break;
+    }
   }
 }
 
--- src/main.h
+++ src/main.h
@@ -2,4 +2,11 @@
 
 #define CREATE_COLOR(A, B) init_pair(A, B, COLOR_BLACK)
 
-bool R2_SHOULD_NOT_USE_COLORS = true; 
+typedef struct
+{
+  int x;
+  int y;
+}R2Point;
+
+
+bool R2_SHOULD_NOT_USE_COLORS = true;
