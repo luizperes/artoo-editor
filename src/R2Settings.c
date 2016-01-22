24eb754b0fe18ff805a5b5172f8d6b2068e9ac7c
luizperes <luiz_peres10@hotmail.com>
2016-01-21 17:29:40 -0800

    Included the beggining of the help

--- src/R2Settings.c
+++ src/R2Settings.c
@@ -11,9 +11,16 @@
 #define R2Settings_HELP_OPTION "Use the identifier '--help' for getting information about the settings"
 #define R2Settings_PATTERN "Please execute Artoo with the command: artoo filename"
 
+#define R2Settings_HELP_ENABLE_COLORS "--enable-colors\t\tEnable the use of colors by the terminal\n"
+#define R2Settings_HELP_DISABLE_COLORS "--disable-colors\tDisable the use of colors by the terminal\n"
+
+#define R2Settings_FULL_HELP R2Settings_HELP_ENABLE_COLORS  R2Settings_HELP_DISABLE_COLORS 
+
 static char* R2Settings_getVersion();
 static char* R2Settings_getHelpOption();
 static char* R2Settings_getHintAboutInitFile();
+static char* R2Settings_getFullHelp();
+static bool  R2Settings_handleArgs(int argc, char **argv);
 
 bool R2Settings_init(int argc, char **argv)
 {
@@ -22,8 +29,28 @@ bool R2Settings_init(int argc, char **argv)
     printf("%s. %s\n%s\n", R2Settings_getVersion(), R2Settings_getHintAboutInitFile(),  R2Settings_getHelpOption());
     return false;
   }
+  else 
+  {
+    return R2Settings_handleArgs(argc, argv); 
+  }
 
+  return true;
+}
+
+static bool R2Settings_handleArgs(int argc, char **argv)
+{
+  // "R2Settings.h"
   const bool R2_SHOULD_NOT_USE_COLORS = false;
+ 
+  for (int i = 0; i < argc; i++)
+  {
+    if (strcmp(argv[i], "--help") == 0)
+    {
+      printf("\n%s\n", R2Settings_getFullHelp());
+      return false;
+    }
+  } 
+
   return true;
 }
 
@@ -40,4 +67,9 @@ static char* R2Settings_getHelpOption()
 static char* R2Settings_getHintAboutInitFile()
 {
   return R2Settings_PATTERN;
+}
+
+static char* R2Settings_getFullHelp()
+{
+  return R2Settings_FULL_HELP;
 }
--- src/include.h
+++ src/include.h
@@ -13,5 +13,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 
 #endif
